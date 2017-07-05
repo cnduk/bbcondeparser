746e33133c6c564bb7065a8bc652dbea82c9311a
Andy Gelnar <apgelnar@gmail.com>
2017-07-05 17:52:42 +0100

    chore(License): Use MIT license

--- setup.py
+++ setup.py
@@ -15,4 +15,15 @@ setup(
     url="https://github.com/cnduk/bbcondeparser",
     download_url="https://github.com/cnduk/bbcondeparser/tarball/v{}".format(__version__),
     keywords=["bbcode"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Text Processing :: Markup",
+    ],
 )
