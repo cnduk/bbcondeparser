b470a7538e5514d3b8346f716b99abcdaf0fd15e
Andy Gelnar <apgelnar@gmail.com>
2017-07-06 11:30:15 +0100

    chore(pypi): put the description back into pypi
    No idea why it wasn't picking up README.rst anymore, but putting it in long_description seems to
    work.

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup, find_packages
 
 from bbcondeparser import __version__
+
+readme_fname = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(readme_fname, 'rb') as fandle:
+    long_desc = fandle.read().decode('utf-8')
 
 setup(
     name='bbcondeparser',
@@ -9,12 +14,14 @@ setup(
     author='Conde Nast Digital UK',
     author_email='condenet.technical@condenast.co.uk',
     description="parser for Conde Nast BBCode",
+    long_description=long_desc,
     packages=find_packages(),
     install_requires=[],
     tests_require=['mock'],
     url="https://github.com/cnduk/bbcondeparser",
     download_url="https://github.com/cnduk/bbcondeparser/tarball/v{}".format(__version__),
     keywords=["bbcode"],
+    license='MIT',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
