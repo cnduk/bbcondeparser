f04fa498310b19863f0c9e4b9105d6e4325ef915
danelliottpalmer <danelliottpalmer@gmail.com>
2017-11-14 15:31:10 +0000

    test(Test runner): update messages with dope sayings

--- container-test-runner.sh
+++ container-test-runner.sh
@@ -2,8 +2,8 @@ for i in 2.7 3.3 3.4 3.5 3.6
 do
     docker run --rm -i -v "$(pwd)":/workdir python:$i bash /workdir/_container_run_tests.sh \
          > /dev/null 2>&1 \
-         && echo "python $i: OK" \
-         || echo "python $i: NOT OK" \
+         && echo "python $i: Matthew McConaughey says ALRIGHT ALRIGHT ALRIGHT" \
+         || echo "python $i: Shia Labeouf says NO NO NO NOOOOO" \
      &
 done
 
